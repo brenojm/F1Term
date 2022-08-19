073efe7d7eb631af83c9bb245923769296a35c67
Breno Jardim Magrani <breno.jm25@gmail.com>
2022-08-19 18:16:40 -0300

    Rotas feitas

--- f1-term/src/App.js
+++ f1-term/src/App.js
@@ -2,13 +2,16 @@
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Home } from './pages/Home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Home/>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
